e256ca548ac00ac2913bf644564ddb83fa324cba
Im-Humor <orangite@gmail.com>
2023-07-23 16:31:44 -0700

    add comments

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const playerFactory = (name, symbol, active) => {
 
 const player1 = playerFactory("mark", "x", false);
 const player2 = playerFactory("stacy", "o", false);
+
+//player array used to help search through players
+//helpful to find and flip active status
 const playerArray = [player1, player2];
 
+//create gameBoard module
 const gameBoard = (() => {
     const gameSpace = document.querySelector(".game-space");
 
@@ -14,6 +18,7 @@ const gameBoard = (() => {
         console.log("Initializing active player");
     }
 
+    //called onclick, flips active status for all players
     const nextTurn = () => {
         for (x = 0; x < playerArray.length; x++) {
             console.log(`${playerArray[x].name} is set from ${playerArray[x].active} to ${!playerArray[x].active}`)
@@ -22,6 +27,8 @@ const gameBoard = (() => {
         console.log("Next turn");
     }
 
+    //returns the active player by finding which player has active
+    //status set to true
     const whoActive = () => {
         for (x = 0; x < playerArray.length; x++) {
             if (playerArray[x].active === true) {
@@ -31,7 +38,13 @@ const gameBoard = (() => {
         }
     }
 
-    let gameItems = ["x", "x", "o", "x", "o", "x", "o", "x", "x"];
+    //initial array of game items
+    let gameItems = ["", "", "", "", "", "", "", "", ""];
+
+    //writes initial array of gameItems to the DOM
+    //then, adds event listeners to each item square
+    //when each square is clicked, set innertext to whoever is active player
+    //and then toggle active status (i.e. next turn)
     const writeItems = () => {
         for (x = 0; x < gameItems.length; x++) {
             const gameSquare = document.createElement("div");
@@ -43,9 +56,6 @@ const gameBoard = (() => {
         const itemSelection = gameSpace.querySelectorAll(".square");
         itemSelection.forEach((square) => {
             square.addEventListener("click", () => {
-                // need to figure out how game knows whose turn it is
-                // and when it's their turn, their symbol is returned as
-                // currentPlayer.symbol
                 square.innerText = whoActive().symbol;
                 nextTurn();
             })
