bfe2b3687c607309ce0850371ba6c95c862fe8e7
Im-Humor <orangite@gmail.com>
2023-07-24 16:08:16 -0700

    replace switch structure with if else structure due to necessity of logical AND operator

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const gameBoard = (() => {
 
     const initActivePlayer = (player) => {
         player.active = true;
-        console.log("Initializing active player");
     }
 
     //called onclick, flips active status for all players
@@ -24,7 +23,6 @@ const gameBoard = (() => {
             console.log(`${playerArray[x].name} is set from ${playerArray[x].active} to ${!playerArray[x].active}`)
             playerArray[x].active = !playerArray[x].active;
         }
-        console.log("Next turn");
     }
 
     //returns the active player by finding which player has active
@@ -32,7 +30,6 @@ const gameBoard = (() => {
     const whoActive = () => {
         for (x = 0; x < playerArray.length; x++) {
             if (playerArray[x].active === true) {
-                console.log(`${playerArray[x].name} is set to active`);
                 return playerArray[x];
             }
         }
@@ -54,12 +51,15 @@ const gameBoard = (() => {
             gameSpace.appendChild(gameSquare);
         }
         const itemSelection = gameSpace.querySelectorAll(".square");
-        itemSelection.forEach((square) => {
+        itemSelection.forEach((square, index) => {
             //normally I would use an anon function in place of 'eventHandler'
             //but in order to remove it I had to name it
             square.addEventListener("click", function eventHandler(event) {
                 square.innerText = whoActive().symbol;
+                gameItems[index] = whoActive().symbol;
+                console.log(gameItems)
                 nextTurn();
+                gameLogic.checkWin(gameItems);
                 //make square unchangeable after initial click by removing
                 //the event listener
                 event.target.removeEventListener("click", eventHandler);
@@ -75,38 +75,93 @@ const gameBoard = (() => {
     return {initActivePlayer, writeItems, gameItems};
 })()
 
+const gameLogic = (() => {
+    let winner = "";
+    const checkWin = (array) => {
+        if (array[0] === "x" && array[1] === "x" && array[2] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[3] === "x" && array[4] === "x" && array[5] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[6] === "x" && array[7] === "x" && array[8] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[0] === "x" && array[3] === "x" && array[6] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[1] === "x" && array[4] === "x" && array[7] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[2] === "x" && array[5] === "x" && array[8] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[0] === "x" && array[4] === "x" && array[8] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[2] === "x" && array[4] === "x" && array[6] === "x"){
+            winner = player1.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        if (array[0] === "o" && array[1] === "o" && array[2] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[3] === "o" && array[4] === "o" && array[5] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[6] === "o" && array[7] === "o" && array[8] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[0] === "o" && array[3] === "o" && array[6] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[1] === "o" && array[4] === "o" && array[7] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[2] === "o" && array[5] === "o" && array[8] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[0] === "o" && array[4] === "o" && array[8] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+        else if (array[2] === "o" && array[4] === "o" && array[6] === "o"){
+            winner = player2.name;
+            console.log(`Winner is ${winner}`);
+            return;
+        }
+    }
+    return {checkWin};
+})()
 
 
 gameBoard.initActivePlayer(player1);
 gameBoard.writeItems();
-
-//TODO: add logic for checking winner
-
-let winner = "";
-switch(gameBoard.gameItems) {
-    case ["x", "x", "x", "", "", "", "", "", ""]:
-        winner = player1.name;
-        break;
-    case ["", "", "", "x", "x", "x", "", "", ""]:
-        winner = player1.name;
-        break;
-    case ["", "", "", "", "", "", "x", "x", "x"]:
-        winner = player1.name;
-        break;
-    case ["x", "", "", "x", "", "", "x", "", ""]:
-        winner = player1.name;
-        break;
-    case ["", "x", "", "", "x", "", "", "x", ""]:
-        winner = player1.name;
-        break;
-    case ["", "", "x", "", "", "x", "", "", "x"]:
-        winner = player1.name;
-        break;
-    case ["x", "", "", "", "x", "", "", "", "x"]:
-        winner = player1.name;
-        break;
-    case ["", "", "x", "", "x", "", "x", "", ""]:
-        winner = player1.name;
-        break;
-
-}
